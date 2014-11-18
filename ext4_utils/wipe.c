16346a53f7a97b9cf0edc99d6d04f8f080fda393
DHOMD <DHO@dho.im>
2014-11-17 21:48:38 -0500

    Reconcile wipe
    Change-Id: Ide085fa47a265ef8f9163e4242dd722b283d51c5

--- ext4_utils/wipe.c
+++ ext4_utils/wipe.c
@@ -17,8 +17,6 @@
 #include "ext4_utils.h"
 #include "wipe.h"
 
-#if WIPE_IS_SUPPORTED
-
 #if defined(__linux__)
 
 #include <linux/fs.h>
@@ -38,11 +36,7 @@ int wipe_block_device(int fd, s64 len)
 	u64 range[2];
 	int ret;
 
-	if (!is_block_device_fd(fd)) {
-		// Wiping only makes sense on a block device.
-		return 0;
-	}
-
+#ifndef NO_SECURE_DISCARD
 	range[0] = 0;
 	range[1] = len;
 	ret = ioctl(fd, BLKSECDISCARD, &range);
@@ -63,17 +57,18 @@ int wipe_block_device(int fd, s64 len)
 #endif
 	return 0;
 }
-
-#else  /* __linux__ */
-#error "Missing block device wiping implementation for this platform!"
-#endif
-
-#else  /* WIPE_IS_SUPPORTED */
-
+#else
 int wipe_block_device(int fd, s64 len)
 {
-	/* Wiping is not supported on this platform. */
+	warn("Wipe via secure discard suppressed due to bug in EMMC firmware\n");
 	return 1;
 }
+#endif
+#else
+int wipe_block_device(int fd, s64 len)
+{
+	error("wipe not supported on non-linux platforms");
+	return 1;
+}
+#endif
 
-#endif  /* WIPE_IS_SUPPORTED */
